7d2a9ffa578537b8f59aa91ef83ee473a764b245
Saturnyasha <t.akhlamionak@gmail.com>
2022-10-16 18:27:50 +0300

    add 2nd request

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -16,38 +16,8 @@ describe("Test suite", () => {
     it("Second test", async()=>{
         await $("//textarea").click();
         await $("//textarea").addValue("Hello from WebDriver");
-        browser.pause(5000);
         await $("#select2-postform-expiration-container.select2-selection__rendered").click();
-        browser.pause(5000);
         await $('//*[@id="select2-postform-expiration-container"]').click();
-        browser.pause(5000);
         await $("#postform-name.form-control").setValue("helloweb");
-    })
-/*(Bring It On) When performing the task, you need to use the capabilities of Selenium WebDriver, 
-the unit testing framework and the Page Object concept. Automate the following script:
-1. Open https://pastebin.com or a similar service in any browser
-2. Create a New Paste with the following details:
-* Code:
-git config --global user.name "New Sheriff in Town"
-git reset $ (git commit-tree HEAD ^ {tree} -m "Legacy code")
-git push origin master --force
-* Syntax Highlighting: "Bash"
-* Paste Expiration: "10 Minutes"
-* Paste Name / Title: "how to gain dominance among developers"*/
-    it("Third test", async()=>{
-        await $("//textarea").click();
-        await $("//textarea").addValue("git config --global user.name 'New Sheriff in Town' git reset $ (git commit-tree HEAD ^ {tree} -m 'Legacy code') git push origin master --force");
-        browser.pause(100000);
-        await $("#select2-postform-expiration-container.select2-selection__rendered").click();
-        browser.pause(100000);
-        await $('//*[@id="select2-postform-expiration-container"]').click();
-        browser.pause(100000);
-        await $('//*[@id="postform-name"]').click();
-        await $('//*[@id="postform-name"]').addValue("how to gain dominance among developers");
-        browser.pause(100000);
-        await $('#select2-postform-format-container.select2-selection__rendered').click();
-        await $('//*[@id="select2-postform-format-container"]').click();
-    });
-  
-    
+    }) 
 });
